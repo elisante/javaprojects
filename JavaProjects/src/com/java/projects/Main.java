72e4b47dca4ee2b9837c9a6cefac8925e3589c38
zippedsoft <stringbongo@gmail.com>
2015-09-02 10:29:56 -0700

    initial checking

---
+++ JavaProjects/src/com/java/projects/Main.java
@@ -0,0 +1,10 @@
+package com.java.projects;
+
+public class Main {
+	public static void main(String args[]){
+		System.out.println("Hellooooo...");
+		// java is sweet guys
+		System.out.println("yeap");
+	}
+
+}
